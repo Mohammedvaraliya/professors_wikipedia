2395fed90a137c671728e5ec505bc641483e00b2
MohammedVaraliya <mohammedvaraliya2661392@gmail.com>
2023-01-01 13:22:57 +0530

    Add basic models in professors app

--- src/professors/models.py
+++ src/professors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Professor(models.Model):
+    name = models.CharField(max_length=255)
+    personal_information = models.TextField()
+    education = models.TextField()
+    work_and_related_experience = models.TextField()
+    awards_and_honors = models.TextField()
+    activities_hobbies = models.TextField()
+    skills = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    
--- src/professors/views.py
+++ src/professors/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Professors Wikipedia',
+    }
+    return render(request, 'professors/index.html', context)
