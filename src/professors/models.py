40e2dee37a01cc39bc2063f0ed2b9d9f8ccbe296
MohammedVaraliya <mohammedvaraliya2661392@gmail.com>
2023-01-15 21:54:30 +0530

    added professor image field

--- src/professors/models.py
+++ src/professors/models.py
@@ -10,6 +10,8 @@ class Professor(models.Model):
     activities_hobbies = models.TextField()
     skills = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='professor', blank=True, null=True)
+
 
     def __str__(self):
         return self.name
--- src/professors/urls.py
+++ src/professors/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from .views import ProfessorsListView, ProfessorDetailView, ProfessorSearchView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home-template'),
     path('professors/', ProfessorsListView.as_view(), name='professor-list'),
     path('professors/<int:pk>/', ProfessorDetailView.as_view(), name='professor-detail'),
     path('search/', ProfessorSearchView.as_view(), name='professor_search'),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
